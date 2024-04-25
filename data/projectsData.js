9359b40d45a5c8317ee1aae6f47ff4b9b8ccf326
Lucas dos Santos Alves <lucassalves12@gmail.com>
2024-04-25 14:19:14 -0300

    docs: add projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,37 @@
 const projectsData = [
   {
+    title: '.NET 8 & RabbitMQ',
+    description: `Publish and consumer console app`,
+    imgSrc: '',
+    href: '',
+    stack: 'Console • .NET 8 • Docker • RabbitMQ',
+    codeLink: 'https://github.com/lucasdsalves/dotnet-rabbitMq',
+  },
+  {
+    title: '.NET 8 & Azure CosmosDB',
+    description: `CRUD with Azure CosmosDB NoSQL Emulator`,
+    imgSrc: '',
+    href: '',
+    stack: 'WebAPI • .NET 8 • Azure CosmosDB • NoSQL',
+    codeLink: 'https://github.com/lucasdsalves/dotnet-cosmosDb',
+  },
+  {
+    title: '.NET 8 & OpenAI Chat GPT',
+    description: `Interacting with OpenAI ChatGPT`,
+    imgSrc: '',
+    href: '',
+    stack: 'WebAPI • .NET 8 • OpenAI • ChatGPT',
+    codeLink: 'https://github.com/lucasdsalves/dotnet-gpt',
+  },
+  {
+    title: '.NET 6 minimal API',
+    description: `Sample of minimal API use`,
+    imgSrc: '',
+    href: '',
+    stack: 'WebAPI • .NET 6 • Entity Framework Core',
+    codeLink: 'https://github.com/lucasdsalves/minimal-api',
+  },
+  {
     title: 'Personal website 3.0',
     description: ``,
     imgSrc: '/static/images/posts/blog-version-3.png',
